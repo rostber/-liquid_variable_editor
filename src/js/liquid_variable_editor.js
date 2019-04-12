97da849db6a680b79c60c92aeb27a2fb6f2efeac
Vladimir Savelyev <rostber@yandex.ru>
2019-04-12 09:23:28 +0300

    Insert between

--- src/js/liquid_variable_editor.js
+++ src/js/liquid_variable_editor.js
@@ -28,6 +28,8 @@ class LiquidVarsEditor {
     this.elDrop = null
     this.elsText = []
     this.elsLiquid = []
+    this.selectionEl = null
+    this.selectionPoistion = null
   }
   init () {
     this.render()
@@ -71,10 +73,6 @@ class LiquidVarsEditor {
     })
     this.elValue.innerHTML = html
 
-    this.elsText = this.elValue.querySelectorAll('[lve-text]')
-    this.elsLiquid = this.elValue.querySelectorAll('[lve-liquid]')
-    this.elsLiquidRemove = this.elValue.querySelectorAll('[lve-liquid-remove]')
-
     this.bindItems()
   }
   getHtmlWrap () {
@@ -99,52 +97,83 @@ class LiquidVarsEditor {
     return `<span lve-liquid data-variable="${value}" class="${this.options.classLiquid}">${label}<span lve-liquid-remove class="${this.options.classLiquidRemove}">${this.options.htmlLiquidRemove}</span></span>`
   }
   bind () {
-    this.on('onclick', this.elAdd, (evt) => {
+    this.on('click', this.elAdd, (evt) => {
       evt.stopPropagation()
       this.addClass(this.elDrop, this.options.classDropVisible)
     })
-    this.on('onclick', document, (evt) => {
+    this.on('click', document, (evt) => {
       this.removeClass(this.elDrop, this.options.classDropVisible)
     })
 
-    this.on('onclick', this.elDropItems, (evt) => {
+    this.on('click', this.elDropItems, (evt) => {
       this.insertValue(evt.target.dataset.value)
+      this.selectionEl = null
     })
   }
   bindItems () {
-    this.on('onkeydown', this.elsText, (evt) => {
+    this.elsText = this.elValue.querySelectorAll('[lve-text]')
+    this.elsLiquid = this.elValue.querySelectorAll('[lve-liquid]')
+    this.elsLiquidRemove = this.elValue.querySelectorAll('[lve-liquid-remove]')
+
+    this.on('keydown', this.elsText, (evt) => {
       this.options.keydown(evt)
       setTimeout(() => {
         this.updateValue()
       }, 0)
     })
-    this.on('onfocus', this.elsText, (evt) => {
-      this.options.focus(evt)
+    this.on('mousedown mouseup keydown keyup', this.elsText, (evt) => {
+      this.selectionPoistion = this.getCaretPosition(evt.target)
     })
-    this.on('onblur', this.elsText, (evt) => {
+    this.on('focus', this.elsText, (evt) => {
+      this.options.focus(evt)
+      this.selectionEl = evt.target
+    })
+    this.on('blur', this.elsText, (evt) => {
       this.options.blur(evt)
     })
 
-    this.on('onclick', this.elsLiquidRemove, (evt) => {
+    this.on('click', this.elsLiquidRemove, (evt) => {
       evt.stopPropagation()
       const item = evt.target.parentNode
       item.parentNode.removeChild(item)
       this.updateValue()
       this.renderItems()
+      this.selectionEl = null
     })
   }
   on (eventName, els, fn) {
     if (els.length > 0) {
       els.forEach((el) => {
-        el[eventName] = fn
+        this.onEvent(eventName, el, fn)
       })
     }
-    else els[eventName] = fn
+    else this.onEvent(eventName, els, fn)
+  }
+  onEvent (eventNames, el, fn) {
+    eventNames.split(' ').forEach((eventName) => {
+      el.addEventListener(eventName, fn)
+    })
   }
   insertValue (value) {
-    this.options.value += `{{${value}}}`
-    this.renderItems()
-    this.options.change(this.options.value)
+    if (!this.selectionEl) {
+      this.options.value += `{{${value}}}`
+      this.renderItems()
+      this.options.change(this.options.value)
+    } else {
+      // TODO: добавление внутрь text
+      const text = this.selectionEl.innerHTML
+      const textLength = text.length
+      const textFirst = text.slice(0, this.selectionPoistion)
+      const textLast = text.slice(this.selectionPoistion, textLength)
+
+      this.selectionEl.insertAdjacentHTML('beforebegin', this.getHtmlText(textFirst))
+      this.selectionEl.insertAdjacentHTML('afterend', this.getHtmlText(textLast))
+      this.selectionEl.insertAdjacentHTML('afterend', this.getHtmlLiquid(value))
+      this.selectionEl.parentNode.removeChild(this.selectionEl)
+      this.bindItems()
+
+      this.updateValue()
+    }
   }
   updateValue () {
     this.parseContent()
@@ -165,5 +194,29 @@ class LiquidVarsEditor {
   removeClass (el, className) {
     if (el.classList) el.classList.remove(className)
     else el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
+  }
+  getCaretPosition (editableDiv) {
+    let caretPos = 0
+    let sel, range
+    if (window.getSelection) {
+      sel = window.getSelection()
+      if (sel.rangeCount) {
+        range = sel.getRangeAt(0)
+        if (range.commonAncestorContainer.parentNode == editableDiv) {
+          caretPos = range.endOffset
+        }
+      }
+    } else if (document.selection && document.selection.createRange) {
+      range = document.selection.createRange()
+      if (range.parentElement() == editableDiv) {
+        var tempEl = document.createElement("span")
+        editableDiv.insertBefore(tempEl, editableDiv.firstChild)
+        var tempRange = range.duplicate()
+        tempRange.moveToElementText(tempEl)
+        tempRange.setEndPoint("EndToEnd", range)
+        caretPos = tempRange.text.length
+      }
+    }
+    return caretPos
   }
 }
